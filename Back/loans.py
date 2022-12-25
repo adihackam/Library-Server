24fc691bdaa15def6219ce64c1b870a6536fbaa4
Adi Hackam <aaaadi01@gmail.com>
2022-12-25 22:48:27 +0200

    Add names to loan table

--- Back/loans.py
+++ Back/loans.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,  request
 loans = Blueprint('loans',__name__)
-from dbLayer import db, Loans, Books
+from dbLayer import db, Loans, Books, Customers
 import json
 from datetime import date, timedelta
 
@@ -21,6 +21,10 @@ def crude_loans(id=-1):
             daysToAdd = 5
         if book.type == 3:
             daysToAdd = 2
+
+        # For testing of expired loan
+        if book.type == 4:
+            daysToAdd = -1
         return_date = date.today() + timedelta(days=daysToAdd)
        
         newLoan = Loans(customer_id, book_id, loan_date, return_date)
@@ -29,9 +33,14 @@ def crude_loans(id=-1):
         return {"message": "new book loan was added"}
     if request.method == 'GET':
         res = []
-        for loan in Loans.query.all():
+        for loan in Loans.query.join(Customers, Customers.id == Loans.customer_id).\
+            join(Books, Books.id == Loans.book_id).\
+            add_columns(Loans.id, Loans.customer_id, Loans.book_id, Loans.loan_date, Loans.return_date, Customers.name.label("customer_name"), Books.name.label("book_name")).all():
             res.append(
-                {"id": loan.id, "customerId": loan.customer_id, "bookId": loan.book_id, "loanDate": loan.loan_date.strftime('%d/%m/%Y'), "returnDate": loan.return_date.strftime('%d/%m/%Y'), })
+                {"id": loan.id, "customerId": loan.customer_id, "customerName": loan.customer_name, "bookName": loan.book_name, "bookId": loan.book_id, "loanDate": loan.loan_date.strftime('%d/%m/%Y'), "returnDate": loan.return_date.strftime('%d/%m/%Y'), })
+        # for loan in Loans.query.all():
+        #     res.append(
+        #         {"id": loan.id, "customerId": loan.customer_id, "bookId": loan.book_id, "loanDate": loan.loan_date.strftime('%d/%m/%Y'), "returnDate": loan.return_date.strftime('%d/%m/%Y'), })
         return (json.dumps(res))
     if request.method == 'DELETE':
         me=Loans.query.get(id)
@@ -42,7 +51,9 @@ def crude_loans(id=-1):
 @loans.route('/expiredLoans', methods=['GET'])
 def expired_loans():
     res = []
-    for loan in Loans.query.filter(Loans.return_date < date.today()):
-        res.append(
-            {"id": loan.id, "customerId": loan.customer_id, "bookId": loan.book_id, "loanDate": loan.loan_date.strftime('%d/%m/%Y'), "returnDate": loan.return_date.strftime('%d/%m/%Y'), })
+    for loan in Loans.query.join(Customers, Customers.id == Loans.customer_id).\
+        join(Books, Books.id == Loans.book_id).\
+        add_columns(Loans.id, Loans.customer_id, Loans.book_id, Loans.loan_date, Loans.return_date, Customers.name.label("customer_name"), Books.name.label("book_name")).filter(Loans.return_date < date.today()).all():
+            res.append(
+                {"id": loan.id, "customerId": loan.customer_id, "customerName": loan.customer_name, "bookId": loan.book_id, "bookName": loan.book_name, "loanDate": loan.loan_date.strftime('%d/%m/%Y'), "returnDate": loan.return_date.strftime('%d/%m/%Y'), })
     return (json.dumps(res))
